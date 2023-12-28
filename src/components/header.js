932f6029967cf20d9ea87cb0642d4b9176b139b4
ivens magno <ivensmagno@gmail.com>
2023-12-28 03:00:09 -0300

    Coloquei um botão

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "../css/header.css";
-import tatuadora from "../img/tatuadora1.png";
 import Logo from "../img/logoPng.png";
 
 //<img src={tatuadora} alt="Tatuadora Pudim" className="tatuadora" />
@@ -14,25 +13,35 @@ function Header() {
 
   return (
     <header className="header">
-      {/*div butão/texto*/}
+      <div className="menuHamburger">
+        <nav className={isActive ? "nav active" : "nav"}>
+          <a href="/" className="logo">
+            <img className="logo1" src={Logo} alt="Logo" />
+          </a>
+          <button className="hamburger" onClick={toggleHamburger}></button>
+          <ul className="nav-list">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Estilo</a>
+            </li>
+            <li>
+              <a href="#">Serviços</a>
+            </li>
+            <li>
+              <a href="#">Contato</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
 
-
-      {/*div*/}
-      <nav className={isActive ? "nav active" : "nav"}>
-        <a href="/" className="logo">
-          <img className="logo1" src={Logo} alt="Logo" />
-        </a>
-        <button className="hamburger" onClick={toggleHamburger}></button>
-        <ul className="nav-list">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Estilo</a></li>
-          <li><a href="#">Serviços</a></li>
-          <li><a href="#">Contato</a></li>
-        </ul>
-      </nav>
+      <div className="tattooPudim">
+        <h1>Os melhores traços de toda Fortaleza</h1>
+        <a href="!#"><button className="buttonPudim">Vejas as tattos</button></a>
+      </div>
     </header>
   );
 }
 
 export default Header;
-
